766138a80f90a7197d8a712866b294231710f1ea
Evgeny Skarlat <evgeny.skarlat@gmail.com>
2018-05-20 21:37:23 +0300

    Created Update Post Created Create Post

--- app/Http/Controllers/API/PostController.php
+++ app/Http/Controllers/API/PostController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Mockery\Exception;
 
 class PostController extends Controller
 {
@@ -32,12 +34,34 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        try{
+            $validator = Validator::make($request->all(), [
+                "title" => "required",
+                "short_description" => "required",
+                "text" => "required"
+            ]);
+
+            if ($validator->fails()) {
+                throw  new Exception('Validator error: title, short_description and text are required!');
+            }
+
+            $post = new Post($request->all());
+            $post->save();
+            return response([
+                'status' => 'success',
+                'data' => $post
+            ], 200);
+        }catch(\Exception $e) {
+            return response([
+                'status' => 'error',
+                'ErrorMsg' => $e->getMessage()
+            ], 404);
+        }
     }
 
     /**
@@ -65,13 +89,36 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        //
+        try{
+            $validator = Validator::make($request->all(), [
+                "title" => "required",
+                "short_description" => "required",
+                "text" => "required"
+            ]);
+
+            if ($validator->fails()) {
+                throw  new Exception('Validator error: title, short_description and text are required!');
+            }
+
+            $post = Post::findOrFail($id);
+            $post->update($request->all());
+
+            return response([
+                'status' => 'success',
+                'data' => $post
+            ], 200);
+        }catch(\Exception $e) {
+            return response([
+                'status' => 'error',
+                'ErrorMsg' => $e->getMessage()
+            ], 404);
+        }
     }
 
     /**
